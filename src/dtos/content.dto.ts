bd436bcaa0a2c84c0498239217e3d8d48cca2668
verne1997 <sourjya@refactor.academy>
2022-12-28 15:58:46 +0530

    added content dto

---
+++ src/dtos/content.dto.ts
@@ -0,0 +1,15 @@
+import { ContentType } from "src/enums/ContentType.enum";
+import { DtoBase } from "./dtoBase";
+import { UserDto } from "./user.dto";
+
+export class ContentDto extends DtoBase {
+
+    
+    title?: string;
+    type?: ContentType;   /// post and poll only
+    body?: string;
+    imageUrls?: string;
+    videoUrl?: string;
+    users?: UserDto[]
+
+}
